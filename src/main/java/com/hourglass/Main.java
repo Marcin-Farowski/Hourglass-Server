e98b0e08cc1cdc3eea2041e18afc15961321e747
Marcin Farowski <m.farowski@gmail.com>
2024-06-04 22:01:35 +0200

    Minor adjustments

--- src/main/java/com/hourglass/Main.java
+++ src/main/java/com/hourglass/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HourglassApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HourglassApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
