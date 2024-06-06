d3147654659c1d81a785acbaf0bf133f48b884d1
Marcin Farowski <m.farowski@gmail.com>
2024-06-06 21:44:44 +0200

    RoutineController

--- src/main/java/com/hourglass/routine/RoutineController.java
+++ src/main/java/com/hourglass/routine/RoutineController.java
@@ -1,4 +1,14 @@
 package com.hourglass.routine;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RoutineController {
+
+    @GetMapping
+    public List<Routine> getRoutines() {
+        // TODO return routines
+    }
+
 }
