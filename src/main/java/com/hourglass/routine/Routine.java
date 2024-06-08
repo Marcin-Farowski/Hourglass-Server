6dabcf8a8c74ac8de90d5b96688f60447f2be122
Marcin-Farowski <m.farowski@gmail.com>
2024-06-08 22:06:52 +0200

    Routine implementation

--- src/main/java/com/hourglass/routine/Routine.java
+++ src/main/java/com/hourglass/routine/Routine.java
@@ -1,4 +1,25 @@
 package com.hourglass.routine;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Routine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "routine_seq")
+    @SequenceGenerator(name = "routine_seq", sequenceName = "routine_sequence", allocationSize = 1)
+    private int id;
+    @Column(nullable = false)
+    private String name;
+    private LocalTime startTime;
+    private Integer renewalInterval;
+
 }
