d6e8987a5faaada7e91620324c6379c11f4be550
Marcin Farowski <m.farowski@gmail.com>
2024-06-09 22:38:18 +0200

    RoutineRepository

---
+++ src/main/java/com/hourglass/routine/RoutineRepository.java
@@ -0,0 +1,8 @@
+package com.hourglass.routine;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoutineRepository extends JpaRepository<Routine, Integer> {
+}
