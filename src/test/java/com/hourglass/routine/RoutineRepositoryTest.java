d0222eeaef4dc38eac8047c5597f608b743651dc
Marcin Farowski <m.farowski@gmail.com>
2024-07-09 21:52:36 +0200

    RoutineRepositoryTest - findByUser

---
+++ src/test/java/com/hourglass/routine/RoutineRepositoryTest.java
@@ -0,0 +1,68 @@
+package com.hourglass.routine;
+
+import com.hourglass.AbstractTestcontainers;
+import com.hourglass.user.Gender;
+import com.hourglass.user.Role;
+import com.hourglass.user.User;
+import com.hourglass.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class RoutineRepositoryTest extends AbstractTestcontainers {
+
+    @Autowired
+    private RoutineRepository underTest;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByUser() {
+        // Given
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        User user = User.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .dateOfBirth(LocalDate.of(1990, 1, 1))
+                .gender(Gender.MALE)
+                .email("john.doe@example.com")
+                .password(passwordEncoder.encode("password"))
+                .role(Role.USER)
+                .build();
+
+        List<Routine> routines = new ArrayList<>();
+        routines.add(new Routine("Morning Jogging", LocalDateTime.now().minusDays(14).withHour(6).withMinute(0), new TimeInterval(0, 0, 1, 0, 0, 0), user));
+        routines.add(new Routine("Read a Book", LocalDateTime.now().minusDays(14).withHour(18).withMinute(0), new TimeInterval(0, 0, 1, 0, 0, 0), user));
+        routines.add(new Routine("Team Meeting", LocalDateTime.now().minusDays(14).withHour(9).withMinute(0), new TimeInterval(0, 0, 0, 7, 0, 0), user));
+        routines.add(new Routine("Grocery Shopping", LocalDateTime.now().minusDays(14).withHour(17).withMinute(0), new TimeInterval(0, 0, 0, 7, 0, 0), user));
+        routines.add(new Routine("Monthly Report", LocalDateTime.now().minusDays(14).withHour(10).withMinute(0), new TimeInterval(0, 0, 0, 0, 1, 0), user));
+
+        userRepository.save(user);
+        underTest.saveAll(routines);
+
+        // When
+        List<Routine> actual = underTest.findByUser(user);
+
+        //Then
+        assertThat(actual).hasSize(5);
+        assertThat(actual).containsExactlyInAnyOrderElementsOf(routines);
+    }
+}
